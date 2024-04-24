5faa95f815ce66f9db0a3f7f9634c0c502c4079c
WiwiLePancho <127683592+WiwiLePancho@users.noreply.github.com>
2024-04-23 22:23:24 -0400

    Update S4_actividad3.sv
    codigo cambiado

--- Codigos-ELO212/GVI09_Sesion4_2024/Actividad3/S4_actividad3.sv
+++ Codigos-ELO212/GVI09_Sesion4_2024/Actividad3/S4_actividad3.sv
@@ -20,14 +20,48 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module S4_actividad3_optimizada #(parameter M=8)(
+    input logic [M-1:0] A,
+    input logic [M-1:0] B,
+    input logic [1:0] OpCode,
+    output logic [4:0] flags,
+    output logic [M-1:0] Result
+);
+    // Variables internas
+    logic V, C, Z, N, P;
+    logic [M:0] R;
+    always_comb begin
+        case(OpCode)
+            2'b00: R = ~(A | B);  // NOR 
+            2'b01: R = ~(A & B);  // NAND 
+            2'b10: R = A + B;     // SUMA
+            2'b11: R = A - B;     // RESTA 
+            default: R = 0;       // Default 
+        endcase
 
-module S4_actividad3 #(parameter M = 8)(
-    input  logic [M-1:0]    A, B,
-    input  logic [1:0]      OpCode,       
-    output logic [M-1:0]    Result,
-    output logic [4:0]      Flags
-    );
-    
-    
-    
+        Result = R[M-1:0];
+
+        // Flags
+        Z = (Result == 0);  // Z
+
+        N = Result[M-1];  // N
+
+        P = ((^Result) & 1'b1);  // P
+
+        if(OpCode == 2'b10)begin
+            V = ((~A[M-1] & ~B[M-1] & Result[M-1]) | (A[M-1] & B[M-1] & ~Result[M-1]));  // V
+            C = (R[M] == 1);  // C
+        end else if(OpCode == 2'b11)begin
+            V = ((~A[M-1] & B[M-1] & Result[M-1]) | (A[M-1] & ~B[M-1] & ~Result[M-1]));  // V
+            C = (A < B);  // C
+        end else begin
+            V = 0;  // V
+            C = 0;  // C
+        end
+
+    end 
+
+    assign flags = {V, C, Z, N, P};
+
 endmodule
+
