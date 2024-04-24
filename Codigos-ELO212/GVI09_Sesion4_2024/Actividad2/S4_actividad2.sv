27a2dc6ad128fe00a6ebd48e35cf5e9acd3de366
WiwiLePancho <127683592+WiwiLePancho@users.noreply.github.com>
2024-04-23 22:21:25 -0400

    Update S4_actividad2.sv
    modificado

--- Codigos-ELO212/GVI09_Sesion4_2024/Actividad2/S4_actividad2.sv
+++ Codigos-ELO212/GVI09_Sesion4_2024/Actividad2/S4_actividad2.sv
@@ -20,15 +20,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module S4_actividad2 #(parameter N = 32)(
     input  logic            clock, reset,
     input  logic            dec, enable, load,
-    input  logic [N-1:0]    load_ref_value,
+    input  logic [N-1:0]    Load_Ref_value,
     output logic [N-1:0]    counterN,
-    output logic            threshold
+	output logic            threshold
     );
     
-   
+   logic [N-1:0] next_counter;
+
+   always_ff @(posedge clock) begin
+       if (reset)
+         counterN <= 0;
+       else 
+         counterN <= next_counter;
+    end 
+    
+    always_comb begin
+        if (load==1)
+            next_counter= Load_Ref_value;
+        else if (enable==0)
+                next_counter = counterN;
+                
+             else if (dec==1)
+                    next_counter = counterN-1;
+                  else
+                    next_counter = counterN+1; 
+                
+                    
+       if (counterN <= Load_Ref_value)
+            threshold = 0;
+       else
+            threshold =1; 
+            
+    end                 
     
 endmodule
+
