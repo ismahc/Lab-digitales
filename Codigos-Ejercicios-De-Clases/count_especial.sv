601919ab988c6d4fbc3fc9f4755938961c1b9d42
WiwiLePancho <francisco.moragam@sansano.usm.cl>
2024-05-03 16:21:13 -0400

    Actualización

--- Codigos-Ejercicios-De-Clases/count_especial.sv
+++ Codigos-Ejercicios-De-Clases/count_especial.sv
@@ -3,13 +3,21 @@ module count_especial (
     input logic clk, rst, ctrl;
     output logic [3:0] count;
 );
+    logic [3:0] count_aux, count_next;
 
-    always @(posedge clk) begin
-        if (rst) begin
-            count <= 4'b0000;
-        end else if (ctrl) begin
-            count <= count + 1;
-        end
+    always_ff @(posedge clock ) 
+        count <= count_next;
+
+    always_comb begin
+        case (rst)
+            1'b1: count_next = 4'b0;
+            default: count_next = count_aux;
+        endcase
     end
+
+
+    //recordar que se puede hacer el mux de esta manera (no trivial)
+    //assign count_aux (ctrl == 1'b1)? count + 4'd2 : count + 4'b1; 
+    //
 
 endmodule
