cc3ae3d66880e52bd0b2a8496011a8c71eac3cc0
WiwiLePancho <127683592+WiwiLePancho@users.noreply.github.com>
2024-05-08 16:18:42 -0400

    Update test_count_especial.sv

--- Codigos-Ejercicios-De-Clases/test_count_especial.sv
+++ Codigos-Ejercicios-De-Clases/test_count_especial.sv
@@ -1,18 +1,25 @@
 `timescale 1ns / 1ps
 
-// Modulo escrito en clases
-
-module count_especial {
+module test_count_especial();
     input logic clk, rst, ctrl;
     output logic [3:0] count;
+    
+count_especial DUT(.clk(clk),
+                    .rst(rst),
+                    .ctrl(ctrl)
+                    .count(count));
+    always #1 clk= ~clk;
+    initial begin 
+            clk=0;
+            rst=1;
+            ctrl=1;
+            #10 rst=0;
+            #10 rst=1;
+            #10 rst=0;
+           
+            
+           
+                        
 
-    contador_especial (. clk (clk), .rst (rst), .ctrl (ctrl), .count (count));
-     always @(posedge ) begin
-        
-     end
-
-    initial begin
-        
     end
-}
 endmodule
